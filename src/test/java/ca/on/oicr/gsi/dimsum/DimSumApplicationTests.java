774e4e885fa5bec08cc7f2d1f8512c84e932ad8e
Dillan Cooke <dillan.cooke@oicr.on.ca>
2022-05-13 08:27:52 -0400

    fixup tests

--- src/test/java/ca/on/oicr/gsi/dimsum/DimSumApplicationTests.java
+++ src/test/java/ca/on/oicr/gsi/dimsum/DimSumApplicationTests.java
@@ -3,9 +3,10 @@ package ca.on.oicr.gsi.dimsum;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@Profile("noauth")
+@ActiveProfiles("noauth")
 class DimSumApplicationTests {
 
 	@Test
