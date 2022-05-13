e4ac9e00a148515b67cb854519c924b0930fdaf8
Dillan Cooke <dillan.cooke@oicr.on.ca>
2022-05-13 08:47:13 -0400

    change test config name to avoid unintentionally using external application.properties

--- src/test/java/ca/on/oicr/gsi/dimsum/DimSumApplicationTests.java
+++ src/test/java/ca/on/oicr/gsi/dimsum/DimSumApplicationTests.java
@@ -2,10 +2,9 @@ package ca.on.oicr.gsi.dimsum;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(properties = {"spring.config.name=test"})
 @ActiveProfiles("noauth")
 class DimSumApplicationTests {
 
