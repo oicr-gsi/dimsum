d926f328e3f3a17f86a8c59c44e49dd3356aaa49
Dillan Cooke <dillan.cooke@oicr.on.ca>
2022-06-14 15:38:03 -0400

    [GLT-3682] made all data classes immutable (#10)

--- src/main/java/ca/on/oicr/gsi/dimsum/data/Requisition.java
+++ src/main/java/ca/on/oicr/gsi/dimsum/data/Requisition.java
@@ -1,61 +1,100 @@
 package ca.on.oicr.gsi.dimsum.data;
 
+import static java.util.Collections.emptyList;
+import static java.util.Collections.unmodifiableList;
+import static java.util.Objects.requireNonNull;
+
 import java.util.List;
 
+import javax.annotation.concurrent.Immutable;
+
+@Immutable
 public class Requisition {
 
-  private long id;
-  private String name;
-  private boolean stopped;
-  private List<RequisitionQc> informationReviews;
-  private List<RequisitionQc> draftReports;
-  private List<RequisitionQc> finalReports;
+  private final long id;
+  private final String name;
+  private final boolean stopped;
+  private final List<RequisitionQc> informaticsReviews;
+  private final List<RequisitionQc> draftReports;
+  private final List<RequisitionQc> finalReports;
+
+  private Requisition(Builder builder) {
+    this.id = requireNonNull(builder.id);
+    this.name = requireNonNull(builder.name);
+    this.stopped = builder.stopped;
+    this.informaticsReviews = builder.informaticsReviews == null ? emptyList()
+        : unmodifiableList(builder.informaticsReviews);
+    this.draftReports = builder.draftReports == null ? emptyList()
+        : unmodifiableList(builder.draftReports);
+    this.finalReports = builder.finalReports == null ? emptyList()
+        : unmodifiableList(builder.finalReports);
+  }
 
   public long getId() {
     return id;
-  }
-
-  public void setId(long id) {
-    this.id = id;
   }
 
   public String getName() {
     return name;
   }
 
-  public void setName(String name) {
-    this.name = name;
-  }
-
   public boolean isStopped() {
     return stopped;
   }
 
-  public void setStopped(boolean stopped) {
-    this.stopped = stopped;
-  }
-
   public List<RequisitionQc> getInformationReviews() {
-    return informationReviews;
-  }
-
-  public void setInformationReviews(List<RequisitionQc> informationReviews) {
-    this.informationReviews = informationReviews;
+    return informaticsReviews;
   }
 
   public List<RequisitionQc> getDraftReports() {
     return draftReports;
   }
 
-  public void setDraftReports(List<RequisitionQc> draftReports) {
-    this.draftReports = draftReports;
-  }
-
   public List<RequisitionQc> getFinalReports() {
     return finalReports;
   }
 
-  public void setFinalReports(List<RequisitionQc> finalReports) {
-    this.finalReports = finalReports;
+  public static class Builder {
+
+    private long id;
+    private String name;
+    private boolean stopped;
+    private List<RequisitionQc> informaticsReviews;
+    private List<RequisitionQc> draftReports;
+    private List<RequisitionQc> finalReports;
+
+    public Builder id(long id) {
+      this.id = id;
+      return this;
+    }
+
+    public Builder name(String name) {
+      this.name = name;
+      return this;
+    }
+
+    public Builder stopped(boolean stopped) {
+      this.stopped = stopped;
+      return this;
+    }
+
+    public Builder informaticsReviews(List<RequisitionQc> informaticsReviews) {
+      this.informaticsReviews = informaticsReviews;
+      return this;
+    }
+
+    public Builder draftReports(List<RequisitionQc> draftReports) {
+      this.draftReports = draftReports;
+      return this;
+    }
+
+    public Builder finalReports(List<RequisitionQc> finalReports) {
+      this.finalReports = finalReports;
+      return this;
+    }
+
+    public Requisition build() {
+      return new Requisition(this);
+    }
   }
 }
--- src/main/java/ca/on/oicr/gsi/dimsum/data/Sample.java
+++ src/main/java/ca/on/oicr/gsi/dimsum/data/Sample.java
@@ -1,133 +1,192 @@
 package ca.on.oicr.gsi.dimsum.data;
 
+import static java.util.Objects.requireNonNull;
+
 import java.time.LocalDate;
 
+import javax.annotation.concurrent.Immutable;
+
+@Immutable
 public class Sample {
 
-  private String id;
-  private String name;
-  private String tissueOrigin;
-  private String tissueType;
-  private String timepoint;
-  private String groupId;
-  private String targetedSequencing;
-  private Boolean qcPassed;
-  private String qcReason;
-  private String qcUser;
-  private LocalDate qcDate;
-  private Boolean dataReviewPassed;
-  private String dataReviewUser;
-  private LocalDate dataReviewDate;
+  private final String id;
+  private final String name;
+  private final String tissueOrigin;
+  private final String tissueType;
+  private final String timepoint;
+  private final String groupId;
+  private final String targetedSequencing;
+  private final Boolean qcPassed;
+  private final String qcReason;
+  private final String qcUser;
+  private final LocalDate qcDate;
+  private final Boolean dataReviewPassed;
+  private final String dataReviewUser;
+  private final LocalDate dataReviewDate;
+
+  private Sample(Builder builder) {
+    this.id = requireNonNull(builder.id);
+    this.name = requireNonNull(builder.name);
+    this.tissueOrigin = requireNonNull(builder.tissueOrigin);
+    this.tissueType = requireNonNull(builder.tissueType);
+    this.timepoint = builder.timepoint;
+    this.groupId = builder.groupId;
+    this.targetedSequencing = builder.targetedSequencing;
+    this.qcPassed = builder.qcPassed;
+    this.qcReason = builder.qcReason;
+    this.qcUser = builder.qcUser;
+    this.qcDate = builder.qcDate;
+    this.dataReviewPassed = builder.dataReviewPassed;
+    this.dataReviewUser = builder.dataReviewUser;
+    this.dataReviewDate = builder.dataReviewDate;
+  }
 
   public String getId() {
     return id;
-  }
-
-  public void setId(String id) {
-    this.id = id;
   }
 
   public String getName() {
     return name;
   }
 
-  public void setName(String name) {
-    this.name = name;
-  }
-
   public String getTissueOrigin() {
     return tissueOrigin;
-  }
-
-  public void setTissueOrigin(String tissueOrigin) {
-    this.tissueOrigin = tissueOrigin;
   }
 
   public String getTissueType() {
     return tissueType;
   }
 
-  public void setTissueType(String tissueType) {
-    this.tissueType = tissueType;
-  }
-
   public String getTimepoint() {
     return timepoint;
-  }
-
-  public void setTimepoint(String timepoint) {
-    this.timepoint = timepoint;
   }
 
   public String getGroupId() {
     return groupId;
   }
 
-  public void setGroupId(String groupId) {
-    this.groupId = groupId;
-  }
-
   public String getTargetedSequencing() {
     return targetedSequencing;
-  }
-
-  public void setTargetedSequencing(String targetedSequencing) {
-    this.targetedSequencing = targetedSequencing;
   }
 
   public Boolean getQcPassed() {
     return qcPassed;
   }
 
-  public void setQcPassed(Boolean qcPassed) {
-    this.qcPassed = qcPassed;
-  }
-
   public String getQcReason() {
     return qcReason;
-  }
-
-  public void setQcReason(String qcReason) {
-    this.qcReason = qcReason;
   }
 
   public String getQcUser() {
     return qcUser;
   }
 
-  public void setQcUser(String qcUser) {
-    this.qcUser = qcUser;
-  }
-
   public LocalDate getQcDate() {
     return qcDate;
-  }
-
-  public void setQcDate(LocalDate qcDate) {
-    this.qcDate = qcDate;
   }
 
   public Boolean getDataReviewPassed() {
     return dataReviewPassed;
   }
 
-  public void setDataReviewPassed(Boolean dataReviewPassed) {
-    this.dataReviewPassed = dataReviewPassed;
-  }
-
   public String getDataReviewUser() {
     return dataReviewUser;
-  }
-
-  public void setDataReviewUser(String dataReviewUser) {
-    this.dataReviewUser = dataReviewUser;
   }
 
   public LocalDate getDataReviewDate() {
     return dataReviewDate;
   }
 
-  public void setDataReviewDate(LocalDate dataReviewDate) {
-    this.dataReviewDate = dataReviewDate;
+  public static class Builder {
+
+    private String id;
+    private String name;
+    private String tissueOrigin;
+    private String tissueType;
+    private String timepoint;
+    private String groupId;
+    private String targetedSequencing;
+    private Boolean qcPassed;
+    private String qcReason;
+    private String qcUser;
+    private LocalDate qcDate;
+    private Boolean dataReviewPassed;
+    private String dataReviewUser;
+    private LocalDate dataReviewDate;
+
+    public Builder id(String id) {
+      this.id = id;
+      return this;
+    }
+
+    public Builder name(String name) {
+      this.name = name;
+      return this;
+    }
+
+    public Builder tissueOrigin(String tissueOrigin) {
+      this.tissueOrigin = tissueOrigin;
+      return this;
+    }
+
+    public Builder tissueType(String tissueType) {
+      this.tissueType = tissueType;
+      return this;
+    }
+
+    public Builder timepoint(String timepoint) {
+      this.timepoint = timepoint;
+      return this;
+    }
+
+    public Builder groupId(String groupId) {
+      this.groupId = groupId;
+      return this;
+    }
+
+    public Builder targetedSequencing(String targetedSequencing) {
+      this.targetedSequencing = targetedSequencing;
+      return this;
+    }
+
+    public Builder qcPassed(Boolean qcPassed) {
+      this.qcPassed = qcPassed;
+      return this;
+    }
+
+    public Builder qcReason(String qcReason) {
+      this.qcReason = qcReason;
+      return this;
+    }
+
+    public Builder qcUser(String qcUser) {
+      this.qcUser = qcUser;
+      return this;
+    }
+
+    public Builder qcDate(LocalDate qcDate) {
+      this.qcDate = qcDate;
+      return this;
+    }
+
+    public Builder dataReviewPassed(Boolean dataReviewPassed) {
+      this.dataReviewPassed = dataReviewPassed;
+      return this;
+    }
+
+    public Builder dataReviewUser(String dataReviewUser) {
+      this.dataReviewUser = dataReviewUser;
+      return this;
+    }
+
+    public Builder dataReviewDate(LocalDate dataReviewDate) {
+      this.dataReviewDate = dataReviewDate;
+      return this;
+    }
+
+    public Sample build() {
+      return new Sample(this);
+    }
+
   }
 }
