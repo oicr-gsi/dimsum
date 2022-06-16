6f9588cfd2bfebcd32fe88c085dc1c2086b5e8b2
Wilbur Zhang <58961173+Arcslogger@users.noreply.github.com>
2022-06-16 14:59:38 -0400

    [GLT-3700] auto populate release version in footer (#14)
    * [GLT-3700] auto populate release version in footer
    
    * fixed test properties
    
    Co-authored-by: Dillan Cooke <dillan.cooke@oicr.on.ca>

--- src/main/java/ca/on/oicr/gsi/dimsum/controller/mvc/CommonModelAttributeProvider.java
+++ src/main/java/ca/on/oicr/gsi/dimsum/controller/mvc/CommonModelAttributeProvider.java
@@ -2,18 +2,27 @@ package ca.on.oicr.gsi.dimsum.controller.mvc;
 
 import ca.on.oicr.gsi.dimsum.service.CaseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
-@ControllerAdvice(basePackages = {"ca.on.oicr.gsi.dimsum.controller.mvc"})
+@ControllerAdvice(basePackages = { "ca.on.oicr.gsi.dimsum.controller.mvc" })
 public class CommonModelAttributeProvider {
 
   @Autowired
   private CaseService caseService;
 
+  @Value("${build.version}")
+  private String buildVersion;
+
   @ModelAttribute("dataAgeMinutes")
   public long getDataAgeMinutes() {
     return caseService.getDataAge().toMinutes();
+  }
+
+  @ModelAttribute("buildVersion")
+  public String getBuildVersion() {
+    return buildVersion;
   }
 
 }
--- src/test/java/ca/on/oicr/gsi/dimsum/DimSumApplicationTests.java
+++ src/test/java/ca/on/oicr/gsi/dimsum/DimSumApplicationTests.java
@@ -4,8 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(properties = {"spring.config.name=test"})
-@ActiveProfiles("noauth")
+@SpringBootTest
+@ActiveProfiles({"noauth", "test"})
 class DimSumApplicationTests {
 
 	@Test
