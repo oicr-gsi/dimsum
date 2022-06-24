3cc1d1877b9d4e952a67935f6200cc38e4349ad0
Dillan Cooke <dillan.cooke@oicr.on.ca>
2022-06-24 16:09:12 -0400

    removed jQuery

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,30 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
-  mode: 'production',
+  mode: "production",
   entry: {
-    common: './ts/common.ts',
-    index: './ts/index.ts'
+    index: "./ts/index.ts",
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
+        use: "ts-loader",
         exclude: /node_modules/,
       },
     ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: [".tsx", ".ts", ".js"],
   },
   output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'target/classes/static/js/'),
+    filename: "[name].js",
+    path: path.resolve(__dirname, "target/classes/static/js/"),
     library: {
-      name: '[name]',
-      type: 'var'
+      name: "[name]",
+      type: "var",
     },
-    clean: true
+    clean: true,
   },
-  plugins: [
-    new webpack.ProvidePlugin({
-      $: 'jquery/src/jquery',
-      jQuery: 'jquery/src/jquery'
-    })
-  ]
 };
