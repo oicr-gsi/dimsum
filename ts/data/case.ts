26349b2f72b08aff97fa789f8a1f76f1d058abae
Dillan Cooke <dillan.cooke@oicr.on.ca>
2022-06-29 12:51:59 -0400

    [GLT-3716] added all missing columns to the cases table

--- ts/data/case.ts
+++ ts/data/case.ts
@@ -12,14 +12,71 @@ export interface Donor {
   externalName: string;
 }
 
+export interface Run {
+  name: string;
+}
+
+export interface Sample {
+  id: number;
+  name: string;
+  tissueOrigin: string;
+  tissueType: string;
+  timepoint: string;
+  groupId: string;
+  targetedSequencing: string;
+  createdDate: string;
+  run: Run;
+  qcPassed: boolean;
+  qcReason: string;
+  qcUser: string;
+  qcDate: string;
+  dataReviewPassed: boolean;
+  dataReviewUser: string;
+  dataReviewDate: string;
+  latestActivityDate: string;
+}
+
 export interface Test {
   name: string;
+  tissueOrigin: string;
+  tissueType: string;
+  timepoint: string;
+  groupId: string;
+  targetedSequencing: string;
+  extractions: Sample[];
+  libraryPreparations: Sample[];
+  libraryQualifications: Sample[];
+  fullDepthSequencings: Sample[];
+  latestActivityDate: string;
+}
+
+export interface RequisitionQc {
+  qcPassed: boolean;
+  qcUser: string;
+  qcDate: string;
+}
+
+export interface Requisition {
+  id: number;
+  name: string;
+  informaticsReviews: RequisitionQc[];
+  draftReports: RequisitionQc[];
+  finalReports: RequisitionQc[];
 }
 
 export interface Case {
   projects: Project[];
   donor: Donor;
+  assayName: string;
+  assayDescription: string;
+  tissueOrigin: string;
+  tissueType: string;
+  timepoint: string;
+  stopped: boolean;
+  receipts: Sample[];
+  earliestReceiptDate: string;
   tests: Test[];
+  requisitions: Requisition[];
   latestActivityDate: string;
 }
 
@@ -51,17 +108,341 @@ export const caseDefinition: TableDefinition<Case, Test> = {
       },
     },
     {
+      title: "Assay",
+      addParentContents(kase, fragment) {
+        fragment.appendChild(document.createTextNode(kase.assayName));
+      },
+    },
+    {
+      title: "First Receipt",
+      addParentContents(kase, fragment) {
+        fragment.appendChild(document.createTextNode(kase.earliestReceiptDate));
+      },
+    },
+    {
+      title: "Receipt/Inspection",
+      addParentContents(kase, fragment) {
+        addSampleIcons(kase.receipts, fragment);
+      },
+    },
+    {
       title: "Test",
       child: true,
-      addChildContents(test, fragment) {
+      addChildContents(test, kase, fragment) {
         fragment.appendChild(document.createTextNode(test.name));
       },
     },
     {
-      title: "Last Activity",
+      title: "Extraction",
+      child: true,
+      addChildContents(test, kase, fragment) {
+        addSampleIcons(test.extractions, fragment);
+        if (
+          samplePhaseComplete(kase.receipts) &&
+          samplePhasePendingWork(test.extractions)
+        ) {
+          if (test.extractions.length) {
+            addSpace(fragment);
+          }
+          addConstructionIcon("extraction", fragment);
+        }
+      },
+    },
+    {
+      title: "Library Preparation",
+      child: true,
+      addChildContents(test, kase, fragment) {
+        addSampleIcons(test.libraryPreparations, fragment);
+        if (
+          samplePhaseComplete(test.extractions) &&
+          samplePhasePendingWork(test.libraryPreparations)
+        ) {
+          if (test.libraryPreparations.length) {
+            addSpace(fragment);
+          }
+          addConstructionIcon("library preparation", fragment);
+        }
+      },
+    },
+    {
+      title: "Library Qualification",
+      child: true,
+      addChildContents(test, kase, fragment) {
+        addSampleIcons(test.libraryQualifications, fragment);
+        if (
+          samplePhaseComplete(test.libraryPreparations) &&
+          samplePhasePendingWork(test.libraryQualifications)
+        ) {
+          if (test.libraryQualifications.length) {
+            addSpace(fragment);
+          }
+          addConstructionIcon("library qualification", fragment);
+        }
+      },
+    },
+    {
+      title: "Full-Depth Sequencing",
+      child: true,
+      addChildContents(test, kase, fragment) {
+        addSampleIcons(test.fullDepthSequencings, fragment);
+        if (
+          samplePhaseComplete(test.libraryQualifications) &&
+          samplePhasePendingWork(test.fullDepthSequencings)
+        ) {
+          if (test.fullDepthSequencings.length) {
+            addSpace(fragment);
+          }
+          addConstructionIcon("full-depth sequencing", fragment);
+        }
+      },
+    },
+    {
+      title: "Informatics Review",
+      addParentContents(kase, fragment) {
+        const sequencingComplete = kase.tests.every((test) =>
+          samplePhaseComplete(test.fullDepthSequencings)
+        );
+        addRequisitionIcons(
+          kase.requisitions,
+          (requisition) => requisition.informaticsReviews,
+          sequencingComplete,
+          fragment
+        );
+      },
+    },
+    {
+      title: "Draft Report",
+      addParentContents(kase, fragment) {
+        const informaticsComplete = requisitionPhaseComplete(
+          kase.requisitions,
+          (requisition) => requisition.informaticsReviews
+        );
+        addRequisitionIcons(
+          kase.requisitions,
+          (requisition) => requisition.draftReports,
+          informaticsComplete,
+          fragment
+        );
+      },
+    },
+    {
+      title: "Final Report",
+      addParentContents(kase, fragment) {
+        const draftComplete = requisitionPhaseComplete(
+          kase.requisitions,
+          (requisition) => requisition.draftReports
+        );
+        addRequisitionIcons(
+          kase.requisitions,
+          (requisition) => requisition.finalReports,
+          draftComplete,
+          fragment
+        );
+      },
+    },
+    {
+      title: "Latest Activity",
       addParentContents(kase, fragment) {
         fragment.appendChild(document.createTextNode(kase.latestActivityDate));
       },
     },
   ],
 };
+
+function samplePhaseComplete(samples: Sample[]) {
+  // consider incomplete if any are pending QC or data review
+  if (
+    samples.some(
+      (sample) => !sample.qcDate || (sample.run && !sample.dataReviewDate)
+    )
+  ) {
+    return false;
+  } else {
+    // consider complete if at least one is passed QC
+    return samples.some((sample) => sample.qcPassed);
+  }
+}
+
+function samplePhasePendingWork(samples: Sample[]) {
+  // pending if there are no samples
+  if (!samples.length) {
+    return true;
+  }
+  // NOT pending if there are samples needing QC or data review
+  if (
+    samples.some(
+      (sample) => !sample.qcDate || (sample.run && !sample.dataReviewDate)
+    )
+  ) {
+    return false;
+  }
+  // NOT pending if there are samples with passed QC and data review (if applicable)
+  return !samples.some(
+    (sample) => sample.qcPassed && (!sample.run || sample.dataReviewPassed)
+  );
+}
+
+function requisitionPhaseComplete(
+  requisitions: Requisition[],
+  getQcs: (requisition: Requisition) => RequisitionQc[]
+) {
+  return requisitions.every((requisition) => {
+    const qcs = getQcs(requisition);
+    return qcs.length && getLatestRequisitionQc(qcs).qcPassed;
+  });
+}
+
+function addSpace(fragment: DocumentFragment) {
+  fragment.appendChild(document.createTextNode(" "));
+}
+
+function addConstructionIcon(phase: string, fragment: DocumentFragment) {
+  const icon = makeIcon(statuses.construction);
+  icon.title = `Pending ${phase}`;
+  fragment.appendChild(icon);
+}
+
+interface QcStatus {
+  label: string;
+  icon: string;
+  qcComplete: boolean;
+}
+
+type QcStatusKey =
+  | "construction"
+  | "analysis"
+  | "qc"
+  | "dataReview"
+  | "passed"
+  | "failed"
+  | "topUp";
+
+const statuses: Record<QcStatusKey, QcStatus> = {
+  construction: {
+    label: "Pending work",
+    icon: "road-barrier",
+    qcComplete: false,
+  },
+  analysis: {
+    label: "Pending analysis",
+    icon: "hourglass",
+    qcComplete: false,
+  },
+  qc: {
+    label: "Pending QC",
+    icon: "magnifying-glass",
+    qcComplete: false,
+  },
+  dataReview: {
+    label: "Pending data review",
+    icon: "glasses",
+    qcComplete: true,
+  },
+  passed: {
+    label: "Passed",
+    icon: "check",
+    qcComplete: true,
+  },
+  failed: {
+    label: "Failed",
+    icon: "x",
+    qcComplete: true,
+  },
+  topUp: {
+    label: "Top-up required",
+    icon: "fill-drip",
+    qcComplete: true,
+  },
+};
+
+function addSampleIcons(samples: Sample[], fragment: DocumentFragment) {
+  samples.forEach((sample, i) => {
+    const status = getSampleStatus(sample);
+    const icon = makeIcon(status);
+    icon.title =
+      (sample.run ? sample.run.name + " - " : "") +
+      `${sample.name}: ${status.label}`; // TODO: more detailed popup
+    fragment.appendChild(icon);
+    if (i < samples.length - 1) {
+      fragment.appendChild(document.createTextNode(" "));
+    }
+  });
+}
+
+function makeIcon(status: QcStatus) {
+  const icon = document.createElement("i");
+  icon.className = `fa-solid fa-${status.icon}`;
+  return icon;
+}
+
+function getSampleStatus(sample: Sample): QcStatus {
+  const firstStatus = getFirstReviewStatus(sample);
+  if (sample.run && firstStatus.qcComplete) {
+    // run-libraries also have data review
+    if (!sample.dataReviewDate) {
+      return statuses.dataReview;
+    } else if (sample.dataReviewPassed === false) {
+      return statuses.failed;
+    }
+    // if data review is passed, first sign-off status is used
+  }
+  return firstStatus;
+}
+
+function getFirstReviewStatus(sample: Sample) {
+  if (sample.qcPassed === false) {
+    return statuses.failed;
+  } else if (sample.qcPassed === true) {
+    return statuses.passed;
+  } else if (sample.qcReason === "Top-up Required") {
+    return statuses.topUp;
+  } else {
+    return statuses.qc;
+  }
+}
+
+function addRequisitionIcons(
+  requisitions: Requisition[],
+  getQcs: (requisition: Requisition) => RequisitionQc[],
+  previousComplete: boolean,
+  fragment: DocumentFragment
+) {
+  requisitions.forEach((requisition) => {
+    const qcs = getQcs(requisition);
+    if (qcs.length) {
+      const status = getRequisitionQcStatus(qcs, previousComplete);
+      addRequisitionIcon(requisition, status, fragment);
+    } else if (previousComplete) {
+      const status = statuses.qc;
+      addRequisitionIcon(requisition, status, fragment);
+    }
+  });
+}
+
+function getRequisitionQcStatus(
+  qcs: RequisitionQc[],
+  previousComplete: boolean
+) {
+  // return status of latest QC in-case there are multiple
+  if (!qcs.length) {
+    return statuses.qc;
+  }
+  const latest = getLatestRequisitionQc(qcs);
+  return latest.qcPassed ? statuses.passed : statuses.failed;
+}
+
+function getLatestRequisitionQc(qcs: RequisitionQc[]) {
+  return qcs.reduce((previous, current) =>
+    previous.qcDate > current.qcDate ? previous : current
+  );
+}
+
+function addRequisitionIcon(
+  requisition: Requisition,
+  status: QcStatus,
+  fragment: DocumentFragment
+) {
+  const icon = makeIcon(status);
+  icon.title = `${requisition.name}: ${status.label}`; // TODO: more detailed popup
+  fragment.appendChild(icon);
+}
--- ts/util/table-builder.ts
+++ ts/util/table-builder.ts
@@ -5,7 +5,11 @@ export interface ColumnDefinition<ParentType, ChildType> {
   title: string;
   child?: boolean;
   addParentContents?: (object: ParentType, fragment: DocumentFragment) => void;
-  addChildContents?: (object: ChildType, fragment: DocumentFragment) => void;
+  addChildContents?: (
+    object: ChildType,
+    parent: ParentType,
+    fragment: DocumentFragment
+  ) => void;
 }
 
 export interface TableDefinition<ParentType, ChildType> {
@@ -112,7 +116,7 @@ export class TableBuilder<ParentType, ChildType> {
     this.definition.columns.forEach((column, i) => {
       if (column.child) {
         if (children.length) {
-          this.addChildCell(tr, column, children[0], i);
+          this.addChildCell(tr, column, children[0], parent, i);
         } else {
           const td = addCell(tr, i);
           shadeNotApplicable(td);
@@ -132,7 +136,7 @@ export class TableBuilder<ParentType, ChildType> {
         const tr = tbody.insertRow();
         this.definition.columns.forEach((column) => {
           if (column.child) {
-            this.addChildCell(tr, column, child, i);
+            this.addChildCell(tr, column, child, parent, i);
           }
         });
       });
@@ -172,6 +176,7 @@ export class TableBuilder<ParentType, ChildType> {
     tr: HTMLTableRowElement,
     column: ColumnDefinition<ParentType, ChildType>,
     child: ChildType,
+    parent: ParentType,
     index: number
   ) {
     if (!column.addChildContents) {
@@ -181,7 +186,7 @@ export class TableBuilder<ParentType, ChildType> {
     }
     const td = addCell(tr, index);
     const fragment = document.createDocumentFragment();
-    column.addChildContents(child, fragment);
+    column.addChildContents(child, parent, fragment);
     td.appendChild(fragment);
   }
 }
