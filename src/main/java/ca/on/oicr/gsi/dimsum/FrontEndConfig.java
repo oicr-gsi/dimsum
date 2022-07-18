29fac1346629bd490fb5b2e9b61a8fa81839f214
Dillan Cooke <dillan.cooke@oicr.on.ca>
2022-07-18 13:59:09 -0400

    [GLT-3728] added pipeline filter; push filter data to front-end (#34)

--- src/main/java/ca/on/oicr/gsi/dimsum/FrontEndConfig.java
+++ src/main/java/ca/on/oicr/gsi/dimsum/FrontEndConfig.java
@@ -1,11 +1,15 @@
 package ca.on.oicr.gsi.dimsum;
 
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Stream;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import ca.on.oicr.gsi.dimsum.service.filtering.PendingState;
 
 /**
  * This class contains configuration needed on the front-end and is served as a JavaScript file by
- * SiteConfigController
+ * SiteConfigController. Some values are updated by CaseService when new data is loaded
  */
 @Component
 public class FrontEndConfig {
@@ -16,12 +20,29 @@ public class FrontEndConfig {
   @Value("${dashi.url}")
   private String dashiUrl;
 
+  private final List<String> pendingStates =
+      Stream.of(PendingState.values()).map(PendingState::getLabel).toList();
+
+  private Set<String> pipelines;
+
   public String getMisoUrl() {
     return misoUrl;
   }
 
   public String getDashiUrl() {
     return dashiUrl;
+  }
+
+  public List<String> getPendingStates() {
+    return pendingStates;
+  }
+
+  public Set<String> getPipelines() {
+    return pipelines;
+  }
+
+  public void setPipelines(Set<String> pipelines) {
+    this.pipelines = pipelines;
   }
 
 }
--- ts/util/site-config.ts
+++ ts/util/site-config.ts
@@ -1,6 +1,8 @@
 interface SiteConfig {
   misoUrl: string;
   dashiUrl: string;
+  pendingStates: string[];
+  pipelines: string[];
 }
 
 export const siteConfig = (window as any).siteConfig as SiteConfig;
