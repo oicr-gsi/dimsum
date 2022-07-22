66ccb754d10e92820bf067496160d25ec299a24d
Wilbur Zhang <58961173+Arcslogger@users.noreply.github.com>
2022-07-22 15:42:03 -0400

    added instance banner text to header menu (#41)

--- src/main/java/ca/on/oicr/gsi/dimsum/controller/mvc/CommonModelAttributeProvider.java
+++ src/main/java/ca/on/oicr/gsi/dimsum/controller/mvc/CommonModelAttributeProvider.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
-@ControllerAdvice(basePackages = { "ca.on.oicr.gsi.dimsum.controller.mvc" })
+@ControllerAdvice(basePackages = {"ca.on.oicr.gsi.dimsum.controller.mvc"})
 public class CommonModelAttributeProvider {
 
   @Autowired
   private CaseService caseService;
 
+  @Value("${instancename}")
+  private String instanceName;
   @Value("${build.version}")
   private String buildVersion;
   @Value("${bugreport.url}")
@@ -22,11 +24,16 @@ public class CommonModelAttributeProvider {
     return caseService.getDataAge().toMinutes();
   }
 
+  @ModelAttribute("instanceName")
+  public String getInstanceName() {
+    return instanceName;
+  }
+
   @ModelAttribute("buildVersion")
   public String getBuildVersion() {
     return buildVersion;
   }
-  
+
   @ModelAttribute("bugReportUrl")
   public String getJiraLink() {
     return bugReportUrl;
