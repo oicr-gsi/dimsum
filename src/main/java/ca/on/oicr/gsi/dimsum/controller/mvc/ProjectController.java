b8e22a113c9a7b23f1742cfb5e41d9787b503853
Dillan Cooke <dillan.cooke@oicr.on.ca>
2022-08-04 10:17:44 -0400

    [GLT-3743] added MISO links to detail page titles (#54)

--- src/main/java/ca/on/oicr/gsi/dimsum/controller/mvc/ProjectController.java
+++ src/main/java/ca/on/oicr/gsi/dimsum/controller/mvc/ProjectController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ca.on.oicr.gsi.dimsum.FrontEndConfig;
 import ca.on.oicr.gsi.dimsum.controller.NotFoundException;
 import ca.on.oicr.gsi.dimsum.data.Case;
 import ca.on.oicr.gsi.dimsum.service.CaseService;
@@ -19,6 +20,8 @@ public class ProjectController {
 
   @Autowired
   private CaseService caseService;
+  @Autowired
+  private FrontEndConfig frontEndConfig;
 
   @GetMapping("/{projectName}")
   public String getProjectDetailsPage(@PathVariable String projectName, ModelMap model) {
@@ -28,9 +31,14 @@ public class ProjectController {
     }
 
     model.put("title", String.format("%s Project Details", projectName));
+    model.put("titleLink", makeMisoProjectUrl(projectName));
     model.put("detailType", CaseFilterKey.PROJECT.name());
     model.put("detailValue", projectName);
     return "detail";
+  }
+
+  private String makeMisoProjectUrl(String projectName) {
+    return String.format("%s/miso/project/shortname/%s", frontEndConfig.getMisoUrl(), projectName);
   }
 
 }
