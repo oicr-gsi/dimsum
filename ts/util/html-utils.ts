91409fafb7f2847f5441625a301f6005752839ef
Wilbur Zhang <wilbur.zhaang@gmail.com>
2022-08-12 16:54:55 -0400

    WIP

--- ts/util/html-utils.ts
+++ ts/util/html-utils.ts
@@ -105,11 +105,11 @@ export function makeNameDiv(name: string, misoUrl: string, dimsumUrl?: string) {
 
 export function makeTooltip(target: HTMLElement, contents: Node) {
   const tooltipContainer = document.createElement("div");
-  tooltipContainer.className = "inline group";
+  tooltipContainer.className = "inline group relative";
   target.classList.add("group-hover:text-green-200");
   const tooltipContentsContainer = document.createElement("div");
   tooltipContentsContainer.className =
-    "absolute -ml-8 -mt-1 bg-white w-fit h-fit p-2 border-2 border-dotted border-green-200 font-inter font-14 font-medium group-hover:visible group-hover:text-green-200 invisible";
+    "absolute block z-20 -ml-8 -mt-1 bg-white w-fit h-fit p-2 border-2 border-dotted border-green-200 font-inter font-14 font-medium group-hover:visible group-hover:text-green-200 invisible";
   tooltipContentsContainer.appendChild(contents);
   tooltipContainer.appendChild(target);
   tooltipContainer.appendChild(tooltipContentsContainer);
--- ts/util/table-builder.ts
+++ ts/util/table-builder.ts
@@ -126,7 +126,7 @@ export class TableBuilder<ParentType, ChildType> {
     this.container.appendChild(topControlsContainer);
     // tableWrapper allows tooltips to overflow without using additional scrollbars
     const tableWrapper = document.createElement("div");
-    tableWrapper.className = "relative";
+    // tableWrapper.className = "relative";
     const tableContainer = document.createElement("div");
     tableContainer.className = "mt-4 overflow-x-auto overflow-y-hidden";
     this.table = document.createElement("table");
