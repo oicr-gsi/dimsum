d648e5c8cf2e04ddb25f562b60e1620e48a5ce27
mengjenn <jenniffer.meng2002@gmail.com>
2022-09-08 09:51:42 -0400

    Add run list page

---
+++ ts/data/run.ts
@@ -0,0 +1,74 @@
+import { TableDefinition } from "../component/table-builder";
+import { makeNameDiv, addMisoIcon } from "../util/html-utils";
+import { urls } from "../util/urls";
+import { Run, Test } from "./case";
+import { Tooltip } from "../component/tooltip";
+import { siteConfig } from "../util/site-config";
+
+export const runDefinition: TableDefinition<Run, void> = {
+  queryUrl:
+    urls.rest.runs.fullDepthSequencings.name &&
+    urls.rest.runs.libraryQualifications.name,
+  defaultSort: {
+    columnTitle: "Last Activity",
+    descending: true,
+    type: "date",
+  },
+  filters: [
+    {
+      title: "Run",
+      key: "RUN",
+      type: "text",
+      autocompleteUrl: urls.rest.autocomplete.runNames,
+    },
+    {
+      title: "Pending",
+      key: "PENDING",
+      type: "dropdown",
+      values: siteConfig.pendingStates,
+    },
+  ],
+  generateColumns: () => [],
+};
+
+/* FROM THE NOTIFICATION BRANCH
+import { TableDefinition } from "../component/table-builder";
+import { makeNameDiv } from "../util/html-utils";
+import { urls } from "../util/urls";
+import { Run } from "./case";
+
+export const notificationDefinition: TableDefinition<Run, void> = {
+  queryUrl: urls.rest.notifications,
+  defaultSort: {
+    columnTitle: "Completion Date",
+    descending: true,
+    type: "date",
+  },
+  generateColumns(data) {
+    return [
+      {
+        title: "Name",
+        addParentContents(run, fragment) {
+          fragment.appendChild(
+            makeNameDiv(
+              run.name,
+              urls.miso.run(run.name),
+              urls.dimsum.run(run.name)
+            )
+          );
+        },
+        sortType: "text",
+      },
+      {
+        title: "Completion Date",
+        addParentContents(run, fragment) {
+          if (run.completionDate) {
+            fragment.appendChild(document.createTextNode(run.completionDate));
+          }
+        },
+        sortType: "date",
+      },
+    ];
+  },
+};
+*/
