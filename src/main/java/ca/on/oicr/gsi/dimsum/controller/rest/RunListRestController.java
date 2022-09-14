30fda4833d1a7156556e35646733440a93466411
mengjenn <jenniffer.meng2002@gmail.com>
2022-09-14 18:42:56 -0400

    Create rest controller for run list

---
+++ src/main/java/ca/on/oicr/gsi/dimsum/controller/rest/RunListRestController.java
@@ -0,0 +1,36 @@
+package ca.on.oicr.gsi.dimsum.controller.rest;
+
+import static ca.on.oicr.gsi.dimsum.controller.mvc.MvcUtils.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ca.on.oicr.gsi.dimsum.controller.BadRequestException;
+import ca.on.oicr.gsi.dimsum.controller.rest.request.DataQuery;
+import ca.on.oicr.gsi.dimsum.data.Run;
+import ca.on.oicr.gsi.dimsum.service.RunListManager;
+import ca.on.oicr.gsi.dimsum.service.filtering.RunSort;
+import ca.on.oicr.gsi.dimsum.service.filtering.TableData;
+
+@RestController
+@RequestMapping("/rest/run-list")
+public class RunListRestController {
+
+  @Autowired
+  private RunListManager runListManager;
+
+  @PostMapping
+  public TableData<Run> query(@RequestBody DataQuery query) {
+    validateDataQuery(query);
+    RunSort sort = parseSort(query, RunSort::getByLabel);
+    boolean descending = parseDescending(query);
+    if (query.getBaseFilter() != null
+        || (query.getFilters() != null && query.getFilters().size() > 0)) {
+      throw new BadRequestException("Requested invalid filters");
+    }
+    return runListManager.getRuns(query.getPageSize(), query.getPageNumber(), sort, descending);
+  }
+
+}
