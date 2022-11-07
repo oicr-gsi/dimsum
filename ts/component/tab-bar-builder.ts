f7b254d69c574fa9d507a382ed4b293161de3f9e
mengjenn <jenniffer.meng2002@gmail.com>
2022-11-07 13:30:26 -0500

    change to generate and destroy tables

--- ts/component/tab-bar-builder.ts
+++ ts/component/tab-bar-builder.ts
@@ -1,14 +1,15 @@
 import { Pair } from "../util/pair";
+import { getSearchParams, updateUrlParams } from "../util/urls";
 import { TableBuilder } from "./table-builder";
 
 class Tab {
   tabContainer: HTMLButtonElement;
-  table: Pair<TableBuilder<any, any>, string>;
+  table: Pair<any, string>;
   selected: boolean = false;
-  onSelect: (tableContainerId: string) => void;
+  onSelect: (table: Pair<any, string>) => void;
   constructor(
-    table: Pair<TableBuilder<any, any>, string>,
-    onSelect: (tableContainerId: string) => void,
+    table: Pair<any, string>,
+    onSelect: (table: Pair<any, string>) => void,
     selected?: boolean
   ) {
     this.table = table;
@@ -19,11 +20,11 @@ class Tab {
       "font-inter font-medium text-12 text-black bg-white px-2 py-1 rounded-md";
     button.textContent = table.value;
     button.onclick = () => {
+      console.log(`clicked tab ${table.value}`);
       if (!this.selected) {
         this.selected = !this.selected;
-        const tableContainerId = this.table.key.container.id;
         this.styleButton();
-        onSelect(this.table.value);
+        onSelect(this.table);
       }
     };
     this.tabContainer = button;
@@ -56,16 +57,133 @@ class Tab {
 }
 
 export class TabBar {
-  tableBuilders: Pair<TableBuilder<any, any>, string>[]; // table builders and titles
-  currentTable: Pair<TableBuilder<any, any>, string>; // title of the default table
-  container: HTMLElement;
+  tables: Pair<any, string>[]; // where the key is the table definition, and the value is the table title
+  defaultTable: string;
+  tabBarContainer: HTMLElement;
+  tableContainer: HTMLElement;
+  tableTitleContainer?: HTMLElement;
   tabs: Tab[] = [];
 
   constructor(
-    tableBuilders: Pair<TableBuilder<any, any>, string>[],
+    tables: Pair<any, string>[],
     defaultTable: string,
-    containerId: string
+    tabBarContainerId: string,
+    tableContainerId: string,
+    tableTitleContainerId?: string
   ) {
+    // get all containers
+    const tabBarContainer = document.getElementById(tabBarContainerId);
+    if (tabBarContainer === null) {
+      throw Error(`Container ID "${tabBarContainerId}" not found on page`);
+    }
+    this.tabBarContainer = tabBarContainer;
+    const tableContainer = document.getElementById(tableContainerId);
+    if (tableContainer === null) {
+      throw Error(`Container ID "${tableContainerId}" not found on page`);
+    }
+    this.tableContainer = tableContainer;
+    if (tableTitleContainerId) {
+      const tableTitleContainer = document.getElementById(
+        tableTitleContainerId
+      );
+      if (tableTitleContainer === null) {
+        throw Error(
+          `Container ID "${tableTitleContainerId}" not found on page`
+        );
+      }
+      this.tableTitleContainer = tableTitleContainer;
+    }
+    this.defaultTable = defaultTable;
+    this.tables = tables;
+  }
+
+  public build() {
+    const controlsContainer = document.createElement("span");
+    controlsContainer.className =
+      "flex-auto space-x-2 rounded-md px-2 py-2 bg-grey-100 inline-block";
+    // given all the tables and their titles, create the tab bar
+    this.tables.forEach((table, idx) => {
+      const reload = (tb: Pair<any, string>) => {
+        this.reloadTable(tb);
+      };
+      this.tabs.push(
+        new Tab(table, reload, table.value === this.defaultTable ? true : false)
+      );
+      controlsContainer.appendChild(this.tabs[idx].tabContainer);
+    });
+
+    this.tabBarContainer.append(controlsContainer);
+    // build the default table
+    this.tables.forEach((tb) => {
+      if (tb.value === this.defaultTable) {
+        this.reloadTable(tb);
+      }
+    });
+  }
+
+  public reloadTable(table: Pair<any, string>) {
+    console.log(`reloading table, selected: ${table.value}`);
+    this.tabs.forEach((tab) => {
+      if (tab.table.value === table.value) {
+        tab.selected = true;
+        // destroy current table and construct new table
+        this.tableContainer.innerHTML = "";
+        new TableBuilder(
+          table.key,
+          this.tableContainer.id,
+          getSearchParams(),
+          updateUrlParams
+        ).build();
+        if (this.tableTitleContainer) {
+          this.tableTitleContainer.innerHTML = table.value;
+        }
+        console.log(`rebuild table: ${table.value}`);
+      } else {
+        tab.selected = false;
+      }
+      tab.styleButton();
+    });
+  }
+}
+
+/* TRY 2 --------------------------------------------------------------------
+export class TabBar {
+  // tableBuilders: Pair<TableBuilder<any, any>, string>[]; // table builders and titles
+  // currentTable: Pair<TableBuilder<any, any>, string>; // title of the default table
+  tables: Pair<any, string>[];
+  tabBarContainer: HTMLElement;
+  tableContainer: HTMLTableElement;
+  tableTitleContainer: HTMLElement;
+  tabs: Tab[] = [];
+
+  // constructor(
+  //   tableBuilders: Pair<TableBuilder<any, any>, string>[],
+  //   defaultTable: string,
+  //   containerId: string
+  // ) {
+  //   const tabBarContainer = document.getElementsById(tabBarContainerId);
+  //   const container = document.getElementById(containerId);
+  //   if (container === null) {
+  //     throw Error(`Container ID "${containerId}" not found on page`);
+  //   }
+  //   this.tableBuilders = tableBuilders;
+  //   this.currentTable = tableBuilders[0]; // temporarily set current table
+  //   // set the default table as the current table
+  //   tableBuilders.forEach((table) => {
+  //     if (table.value === defaultTable) {
+  //       this.currentTable = table;
+  //     }
+  //   });
+  //   this.container = container;
+  // }
+
+  constructor(
+    tables: Pair<any, string>,
+    tabBarContainerId: string,
+    tableContainerId: string,
+    tabletitleContainerId?: string
+  ) {
+    const tabBarContainer = document.getElementsById(tabBarContainerId);
     const container = document.getElementById(containerId);
     if (container === null) {
       throw Error(`Container ID "${containerId}" not found on page`);
@@ -128,8 +246,9 @@ export class TabBar {
     });
   }
 }
+*/
 
-/*
+/* TRY 1 ---------------------------------------------------------------------
 class Tab {
   tabContainer: HTMLButtonElement;
   tableContainerId: string;
