7201ab5117017fccac94c4849e460862068ff6f4
hyeonjeongkwon <105447603+hyeonjeongkwon@users.noreply.github.com>
2023-03-08 14:44:25 -0500

    .

---
+++ src/main/java/ca/on/oicr/gsi/dimsum/controller/rest/ProjectRestController.java
@@ -0,0 +1,9 @@
+package ca.on.oicr.gsi.dimsum.controller.rest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProjectRestController {
+
+}
