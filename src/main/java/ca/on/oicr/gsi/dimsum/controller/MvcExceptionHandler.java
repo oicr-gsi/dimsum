8bd7e8d5e6ad70841d0f80f1f1b15b7f95fd6ef1
Ally Wu <wuall826@gmail.com>
2023-10-17 14:10:20 -0400

    comments implemented

--- src/main/java/ca/on/oicr/gsi/dimsum/controller/MvcExceptionHandler.java
+++ src/main/java/ca/on/oicr/gsi/dimsum/controller/MvcExceptionHandler.java
@@ -14,23 +14,19 @@ public class MvcExceptionHandler {
 
     @Autowired
     private CommonModelAttributeProvider commonModelAttributeProvider;
+
     private static final Logger logger = LoggerFactory.getLogger(MvcExceptionHandler.class);
 
     @ExceptionHandler(ResponseStatusException.class)
     public ModelAndView handleResponseStatusException(ResponseStatusException ex) {
-        ModelAndView mav = new ModelAndView("error");
-        mav.addObject("errorStatus", ex.getStatus().toString());
-        mav.addObject("errorMessage", ex.getReason());
-
-        mav.addObject("dataAgeMinutes", commonModelAttributeProvider.getDataAgeMinutes());
-        mav.addObject("buildVersion", commonModelAttributeProvider.getBuildVersion());
+        ModelAndView mav = prepareErrorModelAndView(ex.getStatus().value(), ex.getReason(), ex);
 
         if (ex.getStatus().is5xxServerError()) {
-            logger.error(ex.getReason(), ex);
+            logger.error("Error Status: " + ex.getStatus() + ". Error Message: " + ex.getReason(),
+                    ex);
+        } else {
+            logger.info("Error Status: " + ex.getStatus() + ". Error Message: " + ex.getReason());
         }
-
-        logger.info("Error Status: " + ex.getStatus());
-        logger.info("Error Message: " + ex.getReason());
 
         return mav;
     }
@@ -38,8 +34,19 @@ public class MvcExceptionHandler {
     @ExceptionHandler(Exception.class)
     public ModelAndView handleGeneralException(Exception ex) {
         logger.error("Unexpected error", ex);
+        return prepareErrorModelAndView(500, "Unexpected error", ex);
+    }
+
+    private ModelAndView prepareErrorModelAndView(int errorStatus, String errorMessage,
+            Exception ex) {
         ModelAndView mav = new ModelAndView("error");
-        mav.addObject("errorMessage", "Unexpected error");
+        mav.addObject("errorStatus",
+                errorStatus + (ex instanceof ResponseStatusException
+                        ? " " + ((ResponseStatusException) ex).getStatus().getReasonPhrase()
+                        : ""));
+        mav.addObject("errorMessage", errorMessage);
+        mav.addObject("dataAgeMinutes", commonModelAttributeProvider.getDataAgeMinutes());
+        mav.addObject("buildVersion", commonModelAttributeProvider.getBuildVersion());
         return mav;
     }
 }
--- src/main/java/ca/on/oicr/gsi/dimsum/controller/RestExceptionHandler.java
+++ src/main/java/ca/on/oicr/gsi/dimsum/controller/RestExceptionHandler.java
@@ -2,37 +2,47 @@ package ca.on.oicr.gsi.dimsum.controller;
 
 import java.util.HashMap;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.server.ResponseStatusException;
 
 @ControllerAdvice(basePackages = "ca.on.oicr.gsi.dimsum.controller.rest")
 public class RestExceptionHandler {
 
+    private static final Logger logger = LoggerFactory.getLogger(RestExceptionHandler.class);
+
     @ExceptionHandler(ResponseStatusException.class)
     public ResponseEntity<Map<String, String>> handleResponseStatusException(
             ResponseStatusException ex) {
-        Map<String, String> error = new HashMap<>();
-        error.put("status", ex.getStatus().toString());
-        error.put("error", ex.getReason());
-
-        return new ResponseEntity<>(error, ex.getStatus());
+        logException(ex.getStatus().value(), ex.getReason(), ex);
+        return prepareErrorResponse(ex.getStatus().value(), ex.getReason(), ex.getStatus());
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<Map<String, String>> handleGeneralException(Exception ex) {
-        Map<String, String> error = new HashMap<>();
-        error.put("status", "500 INTERNAL_SERVER_ERROR");
-        error.put("error", "Unexpected error");
-
-        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+        int status = HttpStatus.INTERNAL_SERVER_ERROR.value();
+        String message = "Unexpected error";
+        logException(status, message, ex);
+        return prepareErrorResponse(status, message, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    @GetMapping("/error-trigger")
-    public void throwError() {
-        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "This is a triggered error");
+    private void logException(int status, String message, Exception ex) {
+        if (status >= 500) {
+            logger.error("Error Status: " + status + ". Error Message: " + message, ex);
+        } else {
+            logger.info("Error Status: " + status + ". Error Message: " + message);
+        }
+    }
+
+    private ResponseEntity<Map<String, String>> prepareErrorResponse(int status,
+            String errorMessage, HttpStatus httpStatus) {
+        Map<String, String> error = new HashMap<>();
+        error.put("status", String.valueOf(status));
+        error.put("error", errorMessage);
+        return new ResponseEntity<>(error, httpStatus);
     }
 }
