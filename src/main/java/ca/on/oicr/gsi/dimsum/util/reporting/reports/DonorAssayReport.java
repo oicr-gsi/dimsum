667a69c0124b50fe6fbb33ee3326f20f80e820df
Ally Wu <wuall826@gmail.com>
2024-12-11 16:26:22 -0500

    fixup

--- src/main/java/ca/on/oicr/gsi/dimsum/util/reporting/reports/DonorAssayReport.java
+++ src/main/java/ca/on/oicr/gsi/dimsum/util/reporting/reports/DonorAssayReport.java
@@ -2,15 +2,12 @@ package ca.on.oicr.gsi.dimsum.util.reporting.reports;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 import com.fasterxml.jackson.databind.JsonNode;
-import ca.on.oicr.gsi.cardea.data.Assay;
 import ca.on.oicr.gsi.cardea.data.Case;
 import ca.on.oicr.gsi.cardea.data.Project;
-import ca.on.oicr.gsi.cardea.data.Test;
 import ca.on.oicr.gsi.dimsum.controller.BadRequestException;
 import ca.on.oicr.gsi.dimsum.service.CaseService;
 import ca.on.oicr.gsi.dimsum.service.filtering.CompletedGate;
@@ -21,45 +18,26 @@ import ca.on.oicr.gsi.dimsum.util.reporting.ReportSection.TableReportSection;
 
 public class DonorAssayReport extends Report {
 
-  private static class RowData {
-
-    private final Case kase;
-
-    public RowData(Case kase, Test test, Assay assay) {
-      this.kase = kase;
-    }
-
-    public Case getCase() {
-      return kase;
-    }
-
-  }
-
-  private static final ReportSection<RowData> mainSection =
-      new TableReportSection<RowData>("Donor Assay Report", Arrays.asList(
-          Column.forString("Project", x -> x.getCase().getProjects().stream()
+  private static final ReportSection<Case> mainSection =
+      new TableReportSection<Case>("Donor Assay Report", Arrays.asList(
+          Column.forString("Project", kase -> kase.getProjects().stream()
               .map(Project::getName)
               .collect(Collectors.joining(", "))),
-          Column.forString("Pipeline", x -> getProjectPipelines(x.getCase())),
-          Column.forString("Donor", x -> x.getCase().getDonor().getName()),
-          Column.forString("External Name", x -> x.getCase().getDonor().getExternalName()),
-          Column.forString("Assay", x -> x.getCase().getAssayName()),
-          Column.forString("Start Date", x -> x.getCase().getStartDate().toString()),
-          Column.forString("Latest Activity", x -> x.getCase().getLatestActivityDate().toString()),
-          Column.forString("Status", x -> getCaseStatus(x.getCase())),
-          Column.forString("Stop/Pause Reason", x -> getStopPauseReason(x.getCase())))) {
+          Column.forString("Pipeline", DonorAssayReport::getProjectPipelines),
+          Column.forString("Donor", kase -> kase.getDonor().getName()),
+          Column.forString("External Name", kase -> kase.getDonor().getExternalName()),
+          Column.forString("Assay", Case::getAssayName),
+          Column.forString("Start Date", kase -> kase.getStartDate().toString()),
+          Column.forString("Latest Activity", kase -> kase.getLatestActivityDate().toString()),
+          Column.forString("Status", DonorAssayReport::getCaseStatus),
+          Column.forString("Stop/Pause Reason", DonorAssayReport::getStopPauseReason))) {
         @Override
-        public List<RowData> getData(CaseService caseService, JsonNode parameters) {
+        public List<Case> getData(CaseService caseService, JsonNode parameters) {
           Set<String> caseIds = getParameterStringSet(parameters, "caseIds");
           if (caseIds == null || caseIds.isEmpty()) {
             throw new BadRequestException("caseIds parameter missing or empty");
           }
-          List<Case> cases = caseService.getCasesByIds(caseIds);
-          Map<Long, Assay> assaysById = caseService.getAssaysById();
-          return cases.stream()
-              .flatMap(kase -> kase.getTests().stream()
-                  .map(test -> new RowData(kase, test, assaysById.get(kase.getAssayId()))))
-              .collect(Collectors.toList());
+          return caseService.getCasesByIds(caseIds);
         }
       };
 
